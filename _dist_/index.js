dbc64c0643817c700803ba61a7e2e37914463b9b
erikablanco <erikablanco@users.noreply.github.com>
2021-07-19 16:54:52 +0000

    deploy: 8a76e9ff23973d8eafed455b353b3003bc62f162

---
+++ _dist_/index.js
@@ -0,0 +1,43 @@
+import {borremos,  registerImage} from "./lazy.js"
+const urlBase = 'https://randomfox.ca/floof/';
+const container = document.querySelector('#images'); 
+const crear = document.querySelector('#crear');
+const borrar = document.querySelector('#borrar');
+
+
+const fetchUrl = async ()=> {
+    try{
+        
+        //Hacemos fetch de la api
+        const data = await fetch(urlBase);
+        //Convertimos en Json la dara
+        const dataJson = await data.json();
+        //Creamos un div que nos sirve de container y le añadimos una clase
+        const containerImage = document.createElement('div');
+        containerImage.className = 'p-4 containerImage'
+        //creamos una img para nuestras imágenes- le asignamos el atributo src, width y clase
+        const image = document.createElement('img');
+        image.dataset.src = dataJson.image
+        image.className ='mx-auto';
+        image.width = '200';
+    
+
+        
+        //le agregamos el container de la imágen al container principal que está en el html y previamente habíamos seleccioando con un query selector
+        container.append(containerImage)
+ 
+        //agregamos la imágen al container
+        containerImage.append(image)
+           
+        //Instanciamos la api de intersection oberver
+
+        setTimeout(function(){registerImage(containerImage)}, 500)
+   
+    }
+    catch{
+    
+    };
+    
+};
+crear.addEventListener('click', fetchUrl)
+borrar.addEventListener('click', borremos)
